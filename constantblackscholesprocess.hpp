17329e64ae33a87f99d8b2faeeba703ff0efa766
ThorpeDia <massokhna.dia@imt-atlantique.net>
2025-03-24 22:19:33 +0100

    Update constantblackscholesprocess.hpp

--- constantblackscholesprocess.hpp
+++ constantblackscholesprocess.hpp
@@ -3,11 +3,29 @@
 
 namespace QuantLib {
 
-    class ConstantBlackScholesProcess : public StochasticProcess1D {
+    ConstantBlackScholesProcess::ConstantBlackScholesProcess(double underlyingValue,
+                                                             double riskFreeRate,
+                                                             double volatility,
+                                                             double dividend)
+        : StochasticProcess1D(ext::make_shared<EulerDiscretization>()),
+          underlyingValue_(underlyingValue), riskFreeRate_(riskFreeRate),
+          volatility_(volatility), dividend_(dividend)
+    {}
 
-        // your implementation goes here
-        
-    };
+    Real ConstantBlackScholesProcess::x0() const {
+        return underlyingValue_;
+    }
+
+    Real ConstantBlackScholesProcess::drift(Time t, Real x) const {
+        return riskFreeRate_ - dividend_ - 0.5 * volatility_ * volatility_;
+    }
+
+    Real ConstantBlackScholesProcess::diffusion(Time t, Real x) const {
+        return volatility_;
+    }
+
+    Real ConstantBlackScholesProcess::apply(Real x0, Real dx) const {
+        return x0 * std::exp(dx);
+    }
 
 }
-
