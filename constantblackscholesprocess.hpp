f3b3d800cfc2694ed35eb2225f02ef79da1467a2
ThorpeDia <massokhna.dia@imt-atlantique.net>
2025-03-25 23:11:25 +0100

    Update constantblackscholesprocess.hpp

--- constantblackscholesprocess.hpp
+++ constantblackscholesprocess.hpp
@@ -1,31 +1,28 @@
+#ifndef CONSTANTBLACKSCHOLSPROCESS_HPP
+#define CONSTANTBLACKSCHOLSPROCESS_HPP
 
 #include <ql/stochasticprocess.hpp>
 
 namespace QuantLib {
 
-    ConstantBlackScholesProcess::ConstantBlackScholesProcess(double underlyingValue,
-                                                             double riskFreeRate,
-                                                             double volatility,
-                                                             double dividend)
-        : StochasticProcess1D(ext::make_shared<EulerDiscretization>()),
-          underlyingValue_(underlyingValue), riskFreeRate_(riskFreeRate),
-          volatility_(volatility), dividend_(dividend)
-    {}
-
-    Real ConstantBlackScholesProcess::x0() const {
-        return underlyingValue_;
-    }
-
-    Real ConstantBlackScholesProcess::drift(Time t, Real x) const {
-        return riskFreeRate_ - dividend_ - 0.5 * volatility_ * volatility_;
-    }
-
-    Real ConstantBlackScholesProcess::diffusion(Time t, Real x) const {
-        return volatility_;
-    }
-
-    Real ConstantBlackScholesProcess::apply(Real x0, Real dx) const {
-        return x0 * std::exp(dx);
-    }
+    //! Black-Scholes process with constant parameters.
+    class ConstantBlackScholesProcess : public StochasticProcess1D {
+      public:
+        ConstantBlackScholesProcess(double underlyingValue,
+                                    double riskFreeRate,
+                                    double volatility,
+                                    double dividend);
+        Real x0() const override;
+        Real drift(Time t, Real x) const override;
+        Real diffusion(Time t, Real x) const override;
+        Real apply(Real x0, Real dx) const override;
+      private:
+        double underlyingValue_;
+        double riskFreeRate_;
+        double volatility_;
+        double dividend_;
+    };
 
 }
+
+#endif
